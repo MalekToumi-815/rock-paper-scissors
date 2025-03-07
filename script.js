f2507d190d73b3f414b2f9ef7268443896e5e074
MalekToumi-815 <191920560+MalekToumi-815@users.noreply.github.com>
2025-03-07 18:01:20 +0100

    Final touches
    added a restart button with its logic and made the final touches for the styling

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let humanscore = 0
 let computerscore = 0
+let gameover = false
 function getcomputerchoice(){
     let x = Math.random()
     if (x<1/3)
@@ -10,36 +11,71 @@ function getcomputerchoice(){
     return "paper"
 }
 function playround(humanchoice,computerchoice){
-    const result = document.querySelector("#result")
-    result.textContent=""
-    result.innerHTML += "<br>Computer chose: " + computerchoice + "<br>You chose: " + humanchoice;
+    if (!gameover){
+        //selecting html elements
+        const player_choice = document.querySelector("#player")
+        const computer_choice = document.querySelector("#computer")
+        const round = document.querySelector("#round_result")
+        const score = document.querySelector("#score")
+        const game = document.querySelector("#game_result")
+        
+        //positioning the display under the corresponding choice
+        player_choice.innerHTML = "You chose: " + humanchoice;
+        computer_choice.innerHTML = "Computer chose: " + computerchoice;
+        if (humanchoice === "rock")
+            player_choice.style.textAlign = "left";
+        else if (humanchoice === "paper")
+            player_choice.style.textAlign = "center";
+        else 
+            player_choice.style.textAlign = "right";
+
+        if (computerchoice === "rock")
+            computer_choice.style.textAlign = "left";
+        else if (computerchoice === "paper")
+            computer_choice.style.textAlign = "center";
+        else 
+            computer_choice.style.textAlign = "right";
 
 
-    if (computerchoice === humanchoice) {
-        result.innerHTML += "<br>It's a tie!"
-    } else if (
-        (computerchoice === "rock" && humanchoice === "scissors") ||
-        (computerchoice === "scissors" && humanchoice === "paper") ||
-        (computerchoice === "paper" && humanchoice === "rock")
-    ) {
-        result.innerHTML += "<br>Computer wins Round!"
-        computerscore++
-    } else {
-        result.innerHTML += "<br>You win Round!"
-        humanscore++
+
+        if (computerchoice === humanchoice) {
+            round.innerHTML = "It's a tie!"
+        } else if (
+            (computerchoice === "rock" && humanchoice === "scissors") ||
+            (computerchoice === "scissors" && humanchoice === "paper") ||
+            (computerchoice === "paper" && humanchoice === "rock")
+        ) {
+            round.innerHTML = "Computer wins Round!"
+            computerscore++
+        } else {
+            round.innerHTML = "You win Round!"
+            humanscore++
+        }
+        score.innerHTML = "your score: "+humanscore+"<br>computer score: "+computerscore
+        if (humanscore == 5){
+            game.innerHTML = "Player wins the game!!!<br>Press restart to play again"
+            gameover = true
+
+        }
+        if (computerscore == 5){
+            game.innerHTML = "Computer wins the game!!!<br>Press restart to play again"
+            gameover = true
+        }
     }
-    result.innerHTML += "<br>your score: "+humanscore+"<br>computer score: "+computerscore
-    if (humanscore == 5){
-        result.innerHTML += "<br>Player wins the game!!!"
-        humanscore = 0
-        computerscore = 0
-    }
-    if (computerscore == 5){
-        result.innerHTML += "<br>Computer wins the game!!!"
-        humanscore = 0
-        computerscore = 0
-    }
+    else 
+    return;
 }
+function replay(){
+    gameover = false
+    console.log(gameover)
+    humanscore = 0
+    computerscore = 0   
+    document.querySelectorAll("#player, #computer, #round_result, #game_result, #score").forEach(field => {
+        field.innerHTML = "";
+    });
+    document.querySelector("#score").innerHTML = "Your Score: 0<br>Computer score: 0"    
+}
+document.querySelector("#restart").addEventListener("click",() => replay())
 document.querySelector("#rock").addEventListener("click",() => playround("rock",getcomputerchoice()))
 document.querySelector("#paper").addEventListener("click",() => playround("paper",getcomputerchoice()))
 document.querySelector("#scissors").addEventListener("click",() => playround("scissors",getcomputerchoice()))
